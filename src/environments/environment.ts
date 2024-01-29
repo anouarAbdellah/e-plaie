a9120b3cd68ee1308e1ab23d386d8da0a600e49b
anouar <anouarabdellah0@gmail.com>
2024-01-29 22:42:47 +0100

    fixing env

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,7 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  url: 'https://youtest.ma/',
+  url: 'https://youtest.ma',
   production: false
 };
 
